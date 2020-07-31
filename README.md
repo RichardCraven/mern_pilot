bc4c382253c705ea8077e656c7c9e38e1baa898e
RichardCraven <rcraven85@gmail.com>
2020-07-31 14:32:28 -0700

    trying to debug heroku local not building client

---
+++ README.md
@@ -0,0 +1,2 @@
+for local development, go into client package.json and change proxy target to localhost:8080
+to use the heroku db, set it to the deployed url
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 require('dotenv').config({path: __dirname + '/.env'})
 process.env['RANDOM_ID'] = Math.random();
 
-console.log('db name: ', process.env['DATABASE_NAME'])
+// console.log('process.env:', process.env)
+console.log('db name: ', process.env['DATABASE'])
 console.log('token: ', process.env['API_TOKEN'])
 console.log('random Id: ', process.env['RANDOM_ID'])
 console.log('**********', process.env['NODE_ENV'])
@@ -18,29 +19,40 @@ const PORT = process.env.PORT || 8080;
 
 const routes = require('./routes/api')
 
-// const routers = require('./routes/api')
-
-
 const MONGODB_URI = 'mongodb+srv://testUser:Yigsuth34!@cluster0-sa9gt.mongodb.net/Cluster0?retryWrites=true&w=majority'
-
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/something', {
+const LOCAL_MONGODB = 'mongodb://localhost/something'
+console.log('process.env.MONGODB_URI is ', process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI || MONGODB_URI || LOCAL_MONGODB, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
 
 mongoose.connection.on('connected', () => {
-    console.log('Moongoose connected.')
+    console.log('Moongoose connected.', )
 })
+
+
 
 
 // Data Parsing
 app.use(express.json());
-app.use(express.urlencoded({encoded: false}));
+app.use(express.urlencoded({encoded: false, extended: true}));
 
 
 // HTTP request logger
 // app.use(cors())
 app.use(morgan('tiny'));
+console.log('outside')
+if(process.env.NODE_ENV === 'production'){
+    console.log('inside')
+    // I tried using
+    app.use(express.static('client/build'))
+
+    // now I'm trying:
+    // app.use(express.static(__dirname + '/client/build'))
+}
+
+
 app.use('/api', routes)
 app.get('/test', function(req, res){
     res.redirect('/api')
@@ -49,10 +61,6 @@ app.get('/', function(req, res){
     res.redirect('/api')
 })
 
-console.log('outside')
-if(process.env.NODE_ENV === 'production'){
-    console.log('in here??')
-    app.use(express.static('client/build'))
-}
+
 
 app.listen(PORT, console.log(`Server is starting at ${PORT}`));
